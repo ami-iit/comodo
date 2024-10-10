70548f6f8621cf68c5bf41c0fbedaa6cf3e0aa83
Lorenzo Conti <lorenzo.conti@iit.it>
2024-10-10 15:50:18 +0200

    Implemented callbacks. Still work in progress, implemented basic structure for PR.
    - ScoreCallback associates a simulation to a score which can be customly defined
    - TrackerCallback helps to store variable values for either printing or eventual plotting

---
+++ src/comodo/mujocoSimulator/callbacks.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+import mujoco
+
+class Callback(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def on_simulation_start(self) -> None:
+        pass
+
+    @abstractmethod
+    def on_simulation_step(self, t: float, data: mujoco.MjData) -> None:
+        pass
+
+    @abstractmethod
+    def on_simulation_end(self) -> None:
+        pass
+
+
+
+
+class ScoreCallback(Callback):
+    def __init__(self, score_function) -> None:
+        self.score = 0
+        self.history = []
+        self.score_function = score_function
+
+    def on_simulation_start(self) -> None:
+        self.score = 0
+        self.history = []
+
+    def on_simulation_step(self, t: float, data: mujoco.MjData) -> None:
+        score = self.score_function(t, data)
+        self.score += score
+        self.history.append(score)
+
+    def on_simulation_end(self) -> None:
+        print(f"Final score: {self.score}")
+
+
+class TrackerCallback():
+    def __init__(self, tracked_variables: list, print_values: bool = False) -> None:
+        self.tracked_variables = tracked_variables
+        self.print_values = print_values
+
+        self.t = []
+        self.vals = {var: [] for var in tracked_variables}
+
+    def on_simulation_start(self) -> None:
+        pass
+
+    def on_simulation_step(self, t: float, data: mujoco.MjData) -> None:
+        self.t.append(t)
+        for var in self.tracked_variables:
+            try:
+                val = eval(f"data.{var}")
+                self.vals[var].append(val)
+                if self.print_values:
+                    print(f"{self.tracked_variables}: {val}")
+            except:
+                print(f"Error: {self.tracked_variables} not found in data")
+
+    def on_simulation_end(self) -> None:
+        pass
+
+    def get_tracked_values(self):
+        return self.t, self.vals
