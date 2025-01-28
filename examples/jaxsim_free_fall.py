7ee8b344820dcf28a4f35227e99a077f190c70aa
Alessandro Croci <alessandro.croci@iit.it>
2025-01-28 17:48:00 +0100

    Refactor JaxSim free fall example

--- examples/jaxsim_free_fall.py
+++ examples/jaxsim_free_fall.py
@@ -31,18 +31,19 @@ import urllib.request
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
+# Here we set some environment variables
+# Flag to solve MUMPS hanging
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "0"
+# os.environ["JAXSIM_COLLISION_USE_BOTTOM_ONLY"] = "0"
+# os.environ["JAX_PLATFORM_NAME"] = "cpu"
+
 import jax.numpy as jnp
+import jaxsim.api as js
 import matplotlib.pyplot as plt
 import numpy as np
 import rod
 from rod.builder.primitives import BoxBuilder
-
-# Here we set some environment variables
-# Flag to solve MUMPS hanging
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["JAXSIM_COLLISION_USE_BOTTOM_ONLY"] = "1"
-
-import jaxsim.api as js
 
 from comodo.centroidalMPC.centroidalMPC import CentroidalMPC
 from comodo.centroidalMPC.mpcParameterTuning import MPCParameterTuning
@@ -55,7 +56,7 @@ from comodo.TSIDController.TSIDParameterTuning import TSIDParameterTuning
 # %%
 # ==== Set simulation parameters ====
 
-T = 1.0
+T = 10.0
 js_dt = 0.001
 
 
@@ -66,7 +67,7 @@ from jaxsim import VelRepr
 
 rod_sdf = rod.Sdf(
     version="1.10",
-    model=BoxBuilder(x=1, y=1, z=1, mass=1.0, name="box")
+    model=BoxBuilder(x=0.2, y=0.2, z=0.2, mass=1.0, name="box")
     .build_model()
     .add_link()
     .add_inertial()
@@ -83,7 +84,7 @@ model = js.model.JaxSimModel.build_from_model_description(
 # %%
 import jax
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 DOFS = 20
 N_SIM_TIME_STEPS = int(T / js_dt)
 
@@ -98,23 +99,45 @@ import jaxsim
 # %%
 import jaxsim.api as js
 
-xyz = np.array([0.0, 0.0, 0.7])
-rpy = np.array([20.0, 0.0, 0.0]) * np.pi / 180
-xyz_rpy_0 = np.concatenate([xyz, rpy])
+# xyz = np.array([0.0, 0.0, 0.7])
+# rpy = np.array([20.0, 0.0, 0.0]) * np.pi / 180
+# xyz_rpy_0 = np.concatenate([xyz, rpy])
 
+# batched_data = jax.vmap(
+#     lambda xyz_rpy: js.data.JaxSimModelData.build(
+#         model=model,
+#         base_position=xyz_rpy[0:3],
+#         # joint_positions=s,
+#         base_quaternion=jaxlie.SO3.from_rpy_radians(*xyz_rpy[3:]).wxyz,
+#     ),
+#     in_axes=(0),
+# )(
+#     jnp.repeat(xyz_rpy_0[None, ...], BATCH_SIZE, axis=0),
+# )
+
+
+# Generate random data with different initial conditions
+
+# Create a random JAX key.
+key = jax.random.PRNGKey(seed=0)
+
+# Split subkeys for sampling random initial data.
+key, *subkeys = jax.random.split(key=key, num=BATCH_SIZE + 1)
+
+# Create the batched data by sampling the height from [0.5, 0.6] meters.
 batched_data = jax.vmap(
-    lambda xyz_rpy: js.data.JaxSimModelData.build(
+    lambda key: js.data.random_model_data(
         model=model,
-        base_position=xyz_rpy[0:3],
-        # joint_positions=s,
-        base_quaternion=jaxlie.SO3.from_rpy_radians(*xyz_rpy[3:]).wxyz,
-    ),
-    in_axes=(0),
-)(
-    jnp.repeat(xyz_rpy_0[None, ...], BATCH_SIZE, axis=0),
-)
+        key=key,
+        base_pos_bounds=([0, 0, 0.7], [0, 0, 1.2]),
+        base_vel_lin_bounds=(0, 0),
+        base_vel_ang_bounds=(0, 0),
+        velocity_representation=VelRepr.Mixed,
+    )
+)(jnp.vstack(subkeys))
 
-# mapped_batched_torques = batched_torques[:, :, _to_js]
+print("W_p_B(t0)=\n", batched_data.base_position[0:10])
+
 
 jit_step = jax.jit(
     jax.vmap(lambda model, data: js.model.step(model, data), in_axes=(None, 0))
@@ -122,7 +145,7 @@ jit_step = jax.jit(
 
 
 # %%
-xyz_rpy_0
+# xyz_rpy_0
 
 # %%
 # Compile the simulation loop
@@ -171,6 +194,7 @@ now = time.perf_counter()
 
 for idx in range(N_SIM_TIME_STEPS):
     ti = idx * js_dt
+    print(f"t = {ti:.3f} / {N_SIM_TIME_STEPS * js_dt} s", end="\r")
     batched_data = jit_step(
         model,
         batched_data,
@@ -197,7 +221,7 @@ plt.xlabel("Time [s]")
 plt.ylabel("Base Position Z [m]")
 plt.title("Base Position Z over Time for All Batches")
 plt.grid()
-plt.ylim(-0.5, 1.5)
+# plt.ylim(-0.5, 1.5)
 # plt.legend()
 plt.show()
 
@@ -271,7 +295,6 @@ try:
     for idx in range(N_SIM_TIME_STEPS):
         ti = idx * js_dt
         if (ti - t_frame_prev) > (1.0 / framerate):
-            print(f"Rendering t={ti:.3f}")
             t_frame_prev = ti
             _mj_model_helper.set_base_position(
                 position=batched_base_positions[idx, batch_model_idx]
@@ -287,4 +310,80 @@ try:
 finally:
     _recorder.write_video(results_dir / pathlib.Path(current_time + "batched.mp4"))
 
+# %%
+# Visualize a grid of the batched simulations
+
+from comodo.utils.iDynVis import iDynTreeVisualizer
+from comodo.utils.visualize_grid import (
+    generate_equally_spaced_coordinates,
+    quaternion_to_matrix,
+)
+
+# Define the number of boxes and the grid size
+number_of_boxes = BATCH_SIZE
+lengths_table_in_meters = 5
+
+# Initialize the visualizer
+idyntree_viz = iDynTreeVisualizer()
+idyntree_viz.prepare_visualization()
+idyntree_viz.modify_camera(lengths_table_in_meters)
+
+# Create the output directory for screenshots
+output_dir = "./screenshots/"
+os.makedirs(output_dir, exist_ok=True)
+
+# Generate equally spaced coordinates for the grid
+coordinates = generate_equally_spaced_coordinates(
+    n=number_of_boxes, l=lengths_table_in_meters
+)
+
+import rod.urdf.exporter
+
+# Convert the SDF to URDF.
+urdf_string = rod.urdf.exporter.UrdfExporter(pretty=True, indent="    ").to_urdf_string(
+    sdf=rod_sdf
+)
+
+
+# Add the models to the visualizer
+name_of_boxes = []
+for i in range(number_of_boxes):
+    name_i = "box" + str(i)
+    name_of_boxes.append(name_i)
+    idyntree_viz.add_model(
+        joint_name_list=[],  # Assuming the box has no joints
+        base_link="base_link",  # Replace with the actual base link name
+        urdf_string=urdf_string,
+        model_name=name_i,
+    )
+
+# Iterate over each time step
+t_frame_prev = -1.0
+for time_idx in range(N_SIM_TIME_STEPS):
+    ti = time_idx * js_dt
+    if (ti - t_frame_prev) > (1.0 / framerate):
+        t_frame_prev = ti
+        for idx_box, name_i in enumerate(name_of_boxes):
+            coordinates_i = coordinates[idx_box]
+            position = batched_base_positions[time_idx, idx_box]
+            orientation = np.array(
+                batched_base_quaternions[time_idx, idx_box]
+            ).squeeze()
+            # Update the model in the visualizer
+            idyntree_viz.update_model(
+                np.array([]),  # No joint positions for the box
+                jaxlie.SE3.from_rotation_and_translation(
+                    translation=position,
+                    rotation=jaxlie.SO3.from_quaternion_xyzw(orientation[[1, 2, 3, 0]]),
+                ).as_matrix(),
+                model_name=name_i,
+                delta_x=coordinates_i[0],
+                delta_y=coordinates_i[1],
+            )
+
+        # Render and save the frame
+        idyntree_viz.viz.run()
+        idyntree_viz.viz.draw()
+        filename = f"screenshot_{time_idx:04d}.png"
+        idyntree_viz.viz.drawToFile(output_dir + filename)
 # %%
